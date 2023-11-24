977b00e1aad0e44c1d86f8c105fb4003a2aba3be
ArunGokul11 <arunkg64@gmail.com>
2023-11-24 10:35:39 +0530

    simple interest project

--- src/App.js
+++ src/App.js
@@ -5,58 +5,116 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 
-
 function App() {
   // js code
-  const [Principle,setPrinciple]=useState(0);
-  const [Interest,setInterest]=useState(0);
-  const [Rate,setRate]=useState(0);
-  const [Year,setYear]=useState(0);
- 
-  const handleSubmit=(e)=>{
+  const [Principle, setPrinciple] = useState(0);
+  const [Interest, setInterest] = useState(0);
+  const [Rate, setRate] = useState(0);
+  const [Year, setYear] = useState(0);
+  const [isPrinciple, setIsPrinciple] = useState(0);
+  const [isRate, setIsRate] = useState(0);
+  const [isYear, setIsYear] = useState(0);
+
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("==Principle Amount==");
+    console.log("===principle amount====");
     console.log(Principle);
-    console.log("====rate of interest===");
+    console.log("===rate of interest====");
     console.log(Rate);
-    console.log("===year===");
+    console.log("===year====");
     console.log(Year);
-    let result=Principle*Year*Rate/100;
+
+    let result = Principle * Year * Rate / 100;
     console.log(result);
     setInterest(result)
   }
-  function clearValue(){
+  function clearValues() {
     setPrinciple(0);
     setInterest(0);
     setRate(0);
-    setYear(0)
+    setYear(0);
+  }
 
+  const validate = (e) => {
+    const { value, name } = e.target
+
+    if (!!value.match(/^[0-9]+$/)) {   //regular expression :
+      if (name === 'principle') {
+        setPrinciple(value);
+        setIsPrinciple(true)
+      }
+      else if (name === 'rate') {
+        setRate(value);
+        setIsRate(true)
+      }
+      else if (name === 'year') {
+        setYear(value);
+        setIsYear(true)
+      }
+    }
+    else {
+      if (name === 'principle') {
+        setPrinciple(value);
+        setIsPrinciple(false)
+      }
+      else if (name === 'rate') {
+        setRate(value);
+        setIsRate(false)
+      }
+      else if (name === 'year') {
+        setYear(value);
+        setIsYear(false)
+      }
+    }
 
   }
+
+
+
   return (
-    <div className='d-flex justify-content-center align-items-center w-100 mt-5'
-      style={{ height: "70vh" }}>
-      <div style={{ width: "500px" }} className='bg-light p-5 rounded text-center'>
+    <div className="d-flex justify-content-center align-items-center w-100 mt-5" style={{ height: "70vh" }} >
+      <div className="bg-light p-5 rounded text-center mt-5" style={{ width: "500px" }}>
         <h3>SIMPLE INTEREST</h3>
-        <p>Calculate your simple interest</p>
-        <div style={{ height: '150px' }} className='flex-column mt-5 bg-warning d-flex justify-content-center align-items-center'>
+        <p>Calculate Your Simple Interest Easily</p>
+        <div className=" flex-column mt-5 bg-warning d-flex justify-content-center align-items-center" style={{ height: "150px" }}>
           <h1>{'\u20B9'}{Interest}</h1>
           <p>Total Simple Interest</p>
         </div>
-        <form action="" className='mt-5' onSubmit={(e)=>handleSubmit(e)}>
-          <div className='mb-3'>
-            <TextField id="outlined-basic" label="Principle Amount" variant="outlined" className='w-100' value={Principle} onChange={(e)=>setPrinciple(e.target.value)} />
+        <form action="" className='mt-5' onSubmit={(e) => handleSubmit(e)}>
+          <div className='mb-4 text'>
+            <TextField name='principle' id="outlined-basic" label="Principle Amount" variant="outlined" className="w-100" value={Principle}
+              onChange={(e) => validate(e)} />
+
+            {!isPrinciple &&
+              <div>
+                <p className='text-danger '>Invalid input</p>
+              </div>
+            }
           </div>
-          <div className='mt-3'>
-            <TextField id="outlined-basic" label="Rate of Interest(pa)%" variant="outlined" className='w-100' value={Rate} onChange={(e)=>setRate(e.target.value)} />
+
+          <div className='mb-4'>
+            <TextField name='rate' id="outlined-basic" label="Rate Of Interest(pa)%" variant="outlined" className="w-100" value={Rate}
+              onChange={(e) => validate(e)} />
+              {!isRate &&
+              <div>
+                <p className='text-danger '>Invalid input</p>
+              </div>
+            }
           </div>
-          <div className='mt-3'>
-            <TextField id="outlined-basic" label="Year(yr)" variant="outlined" className='w-100' value={Year} onChange={(e)=>setYear(e.target.value)}/>
+          <div className='mb-4 '>
+            <TextField name='year' id="outlined-basic" label="Year(yr)" variant="outlined" className="w-100 border-warning" value={Year}
+              onChange={(e) => validate(e)} />
+              {! isYear &&
+              <div>
+                <p className='text-danger '>Invalid input</p>
+              </div>
+            }
           </div>
-          <div className='mt-3 d-flex justify-content-center'>
+          <div className='mb-4 d-flex justify-content-center '>
             <Stack direction="row" spacing={2}>
-            <Button variant="contained" className='bg-success' type='submit'>Calculate</Button>
-            <Button variant="outlined" className='border-danger text-danger' onClick={clearValue}>Reset</Button>
+              <Button disabled={!isPrinciple || !isRate || !isYear} type='submit' className='bg-success px-4 py-2' variant="contained">Calculate</Button>
+              <Button onClick={clearValues} className='border-danger text-danger' variant="outlined">Reset</Button>
             </Stack>
           </div>
         </form>
